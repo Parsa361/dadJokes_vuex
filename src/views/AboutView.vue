6657bdeace037322ef672338f8ed39c6be34d14f
Parsa Farahani <parsafarahany821@gmail.com>
2023-03-14 19:46:03 +0330

    about us updated

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <h1>This is an about page</h1>
+  <div class="my-5">
+    <div class="shadow-md rounded-md px-3 py-5 bg-white text-center">
+      <h1 class="text-xl font-bold text-gray-900 mb-3">About Us</h1>
+      <p class="text-gray-600 font-medium">Welcome to our web application, "icanhazdadjokes API Joke Fetching". application is built using the Vue.js framework, along with Vuex and TailwindCSS.</p>
+    </div>
   </div>
 </template>
 
